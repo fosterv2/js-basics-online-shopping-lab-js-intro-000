a14b1cf1cf863b9a3b46c863bfce5a4a33839b17
Valerie Foster <fosterv2@outlook.com>
2019-11-06 02:16:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,10 +19,12 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  var line = 'In your cart, you have'
   if (cart.length === 0) {
     return "Your shopping cart is empty."
+  } else if (cart.length === 1) {
+    line = `${line} ${cart[i].itemName} at $${cart[i].itemPrice}.`
   } else {
-    var line = "In your cart, you have"
     for (var i = 0; i < cart.length - 1; i++) {
       line = `${line} ${cart[i].itemName} at $${cart[i].itemPrice},`
     }

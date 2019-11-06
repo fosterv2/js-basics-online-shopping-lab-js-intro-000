88580157437c2e42df8778e978ac30a5b57ea44e
Valerie Foster <fosterv2@outlook.com>
2019-11-06 02:09:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,20 +11,57 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var num = Math.floor(Math.random() * 100) + 1
+ var obItem = { itemName: item, itemPrice: num }
+ cart.push(obItem)
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  } else {
+    var line = "In your cart, you have"
+    for (var i = 0; i < cart.length - 1; i++) {
+      line = `${line} ${cart[i].itemName} at $${cart[i].itemPrice},`
+    }
+    return `${line} and ${cart[i].itemName} at $${cart[i].itemPrice}.`
+  }
 }
 
 function total() {
   // write your code here
+  var num = 0
+  for (var i = 0; i < cart.length; i++) {
+    num = num + cart[i].itemPrice
+  }
+  return num
 }
 
 function removeFromCart(item) {
   // write your code here
+  var count = cart.length
+  var i = 0
+  while (cart.length === count) {
+    if (item === cart[i].itemName) {
+      cart.splice(i, 1)
+    }
+  }
+  if (cart.length === count) {
+    return "That item is not in your cart."
+  } else {
+    return cart
+  }
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber === empty) {
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    var line = `Your total cost is $${tota()}, which will be charged to the card ${cardNumber}`
+    cart.splice(0)
+    return line
+  }
 }
